cbad04f415a91938ef33643b70356726d60b3e66
coolindguy <sraman@hotmail.com>
2018-05-03 06:26:37 -0400

    First Commit

---
+++ src/main/java/com/spicekey/spicekeyspa/SpicekeyspaApplication.java
@@ -0,0 +1,12 @@
+package com.spicekey.spicekeyspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpicekeyspaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpicekeyspaApplication.class, args);
+	}
+}
---
+++ src/main/java/com/spicekey/spicekeyspa/api/SingleClassTest.java
@@ -0,0 +1,28 @@
+package com.spicekey.spicekeyspa.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SingleClassTest {
+
+	static List<String> nameOfEmployees = new ArrayList<String>();
+		
+	static {
+		nameOfEmployees.add("Raman Sharma");
+		nameOfEmployees.add("Anamika Sharma");
+	}
+	
+    @Value("${employee.name}")
+    private String employeeName;
+    
+    @GetMapping("/returnname")
+    public List<String> employeeList(){
+    	nameOfEmployees.add(employeeName);
+        return nameOfEmployees;
+    }
+}
